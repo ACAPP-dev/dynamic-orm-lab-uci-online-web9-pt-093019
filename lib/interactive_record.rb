44768a2ade1d7b9e7acc7486cc660733b235d52f
Andrew Capp <acapp909@gmail.com>
2019-12-19 23:30:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,21 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  def self.column_names
+    sql = "PRAGMA table_info(#{self.table_name})"
+    array_of_hashes = DB[:conn].execute(sql)
+    column_names = []
+    array_of_hashes.each do |hash|
+      column_names << hash["name"]
+    end
+    column_names
+  end
+  def initialize(attributes={})
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
 end
